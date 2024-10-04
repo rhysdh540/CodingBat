8ce30f1a274f7dc31c3de7a74940dea0c12161fe
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-10-04 10:23:28 -0400

    slightly less cursed lol

--- java/Recursion2.java
+++ java/Recursion2.java
@@ -48,17 +48,12 @@ public class Recursion2 {
 	 * be chosen. (No loops needed.)
 	 */
 	public boolean groupSum5(int start, int[] nums, int target) {
-		return start >= nums.length && target == 0
-				|| start < nums.length
-				&& (nums[start] % 5 == 0
-					&& (start < nums.length - 1
-						&& nums[start + 1] == 1
-						&& groupSum5(start + 2, nums, target - nums[start])
-						|| (start >= nums.length - 1 || nums[start + 1] != 1)
-						&& groupSum5(start + 1, nums, target - nums[start]))
-					|| nums[start] % 5 != 0
-					&& (groupSum5(start + 1, nums, target - nums[start])
-						|| groupSum5(start + 1, nums, target)));
+		return start >= nums.length
+				? target == 0
+				: nums[start] % 5 == 0
+					? groupSum5(start + (start < nums.length - 1 && nums[start + 1] == 1 ? 2 : 1), nums, target - nums[start])
+					: groupSum5(start + 1, nums, target - nums[start])
+						|| groupSum5(start + 1, nums, target);
 	}
 
 	/**
